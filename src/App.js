a3e64994b1327d5b451c3547607f1c05d4a5a4ba
elias1991xNataly <eliasalfonso1991@gmail.com>
2023-06-28 15:17:46 +0200

    advanced routing theory

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ import ErrorPage from './pages/ErrorPage';
 import ProjectsPage from './pages/ProjectsPage/ProjectsPage';
 import homePageWithNavigate from "../src/pages/HomePageWithNavigate";
 import projectsData from "./projects-data.json";
-
+import ProjectDetailsPage from './pages/ProjectDetailsPage';
+import QueryStringExample from './pages/queryStringExample';
 
 function App() {
 
@@ -22,7 +23,8 @@ function App() {
         <Route path="/" element={<homePageWithNavigate />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/projects" element={<ProjectsPage projects={projectsData} />} />
-
+        <Route path="projects/:projectId" element={<ProjectDetailsPage />} />
+        <Route path="example" element={<QueryStringExample />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
 
